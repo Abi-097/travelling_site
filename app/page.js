a356280831a7145c5580c24cdca28ca100b40611
Abi-097 <120549516+Abi-097@users.noreply.github.com>
2024-04-13 19:23:17 +0530

    url added

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import Features from "@/components/Features";
 import GetApp from "@/components/GetApp";
 import Guide from "@/components/Guide";
 import Hero from "@/components/Hero";
-
+// https://www.youtube.com/watch?v=cuzw4vL1z5E&t=1s&ab_channel=JavaScriptMastery
 export default function Home() {
   return (
     <>
